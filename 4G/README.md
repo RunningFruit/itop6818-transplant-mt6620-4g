991cfc1b66f96f510e24b9207645eb21534dbe7f
brightereyer2 <brightereyer2@163.com>
2020-04-06 18:23:47 +0800

    *

---
+++ 4G/README.md
@@ -0,0 +1,7 @@
+
+
+```
+chmod a+x placeAll.sh
+sh placeAll.sh
+ping www.baidu.com -c 4 -I ppp0
+```
--- 4G/placeAll.sh
+++ 4G/placeAll.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-chmod +x pppd;chmod +x chat;chmod +x pppd_conf.sh;
+chmod a+x pppd;
+chmod a+x chat;
+chmod a+x pppd_conf.sh;
+chmod a+x wcdma*
 
 cp pppd /usr/sbin/
 cp chat /usr/sbin/
 
-cp wcdma /etc/ppp/peers/
-cp wcdma-chat-connect /etc/ppp/peers/
-cp wcdma-chat-disconnect /etc/ppp/peers/
+mkdir -p /etc/ppp/peers/
+cp wcdma* /etc/ppp/peers/
 
 #建立软连接
 ln /lib/ld-linux-armhf.so.3 /lib/ld-linux.so.3
@@ -15,6 +17,6 @@ ln /lib/ld-linux-armhf.so.3 /lib/ld-linux.so.3
 #测试
 cp pppd_conf.sh /;cd /
 
-./pppd_conf.sh
+sh pppd_conf.sh
 #显示
 #ping www.baidu.com -c 4 -I ppp0
